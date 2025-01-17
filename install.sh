32c9a767d992aa867551fbde6be1c3557445ac2f
Brad Zacher <brad.zacher@gmail.com>
2025-01-17 10:49:44 +1030

    Update install.sh

--- install.sh
+++ install.sh
@@ -18,8 +18,14 @@ brew install \
   thefuck \
   tokei
 
-# install rust
+# install rust and rust deps
 rustup default stable
+curl -L --proto '=https' --tlsv1.2 -sSf https://raw.githubusercontent.com/cargo-bins/cargo-binstall/main/install-from-binstall-release.sh | bash # binstall for installing cargo tools from source
+curl -LsSf https://insta.rs/install.sh | sh            # insta for rust snapshots
+cargo install --git https://github.com/oxalica/nil nil # nil LSP for nix
+cargo binstall cargo-watch                             # watch for re-executing cargo commands on file change
+cargo install cargo-instruments                        # instruments for profiling rust code
+
 
 # install nvm
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh | bash
